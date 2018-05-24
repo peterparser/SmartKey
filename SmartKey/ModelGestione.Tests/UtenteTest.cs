e512df517e2f83ea672a33bce296dfada1855001
Massimo Schembri <massimo.schembri@studio.unibo.it>
2018-05-24 22:18:42 +0200

    Inizializzazione

---
+++ SmartKey/ModelGestione.Tests/UtenteTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace SmartKey.ModelGestione.Tests
+{
+    [TestFixture]
+    class UtenteTest
+    {
+        //Test per verificare la correttezza del pattern Singleton
+        [TestCase]
+        public void TestSingleton()
+        {
+            Utente me = Utente.GetUtente();
+            Utente you = Utente.GetUtente();
+            Assert.AreSame(me, you);
+        }
+
+        [TestCase]
+        public void TestNomeUtente()
+        {
+            Utente me = Utente.GetUtente();
+            Assert.AreEqual(me.NomeUtente, WindowsIdentity.GetCurrent().User.ToString());
+        } 
+        
+        [TestCase]
+        public void TestNomeHost()
+        {
+            Utente me = Utente.GetUtente();
+            Assert.AreEqual(me.NomeHost, WindowsIdentity.GetCurrent().Name.ToString());
+        }
+    }
+}
---
+++ SmartKey/ModelGestione/Utente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+namespace SmartKey.ModelGestione
+{
+    public sealed class Utente
+    {
+        private static Utente _instance;
+        private string nomeHost = WindowsIdentity.GetCurrent().Name.ToString();
+        private string nomeUtente =WindowsIdentity.GetCurrent().User.ToString();
+        private Utente()
+        {
+
+        }
+
+        public static Utente GetUtente()
+        {
+            if(_instance == null)
+            {
+                _instance = new Utente();
+            }
+            return _instance;
+        }
+
+        public string NomeUtente
+        {
+            get
+            {
+                return nomeUtente;
+            }
+        }
+
+        public string NomeHost
+        {
+            get
+            {
+                return nomeHost;
+            }
+        }
+
+    }
+}
