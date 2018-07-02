d59d1264d12a677efcb1f12a9a03b158a52b42c4
Massimo Schembri <massimo.schembri@studio.unibo.it>
2018-07-02 09:44:18 +0200

    Aggiunto e testato il log parsing di: Impostazione Trasferimento e Blacklist

---
+++ SmartKey/Log/LogPersistence/TestPersistenceLog/TestLogParsing.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using SmartKey.Blacklist;
+using SmartKey.Controller.Controller.Interfaces;
+using SmartKey.ImpostazioneTrasferimento;
+using SmartKey.Log.ControllerLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartKey.Log.ModelLog.TestLog
+{
+    [TestFixture]
+    class TestLogParsing
+    {
+        private IController impostazioneController;
+        private IObserver logController;
+        private IController blacklistController;
+
+        private void Init()
+        {
+            blacklistController = new BlackListController();
+            impostazioneController = new ImpostazioneTrasferimentoController();
+            logController = new LogController();
+            impostazioneController.ToLog += logController.UpdateLog;
+            blacklistController.ToLog += logController.UpdateLog;
+
+        }
+
+        [TestCase]
+        public void TestParsingImpostazione()
+        {
+            ((IGestoreImpostazione)impostazioneController).AddImpostazione(
+               new ImpostazioneTrasferimento.ImpostazioneTrasferimento("mydir", "yourDir"));
+            //Prendo la entry che ho appena scritto
+            Entry scritta = logController.Entries[0];
+            if(scritta is EntryImpostazione)
+            {
+                EntryImpostazione  impostazioneEntry = (EntryImpostazione)scritta;
+                Assert.IsTrue(impostazioneEntry.Destinatario.Equals("yourDir"));
+                Assert.IsTrue(impostazioneEntry.Sorgente.Equals("mydir"));
+            }
+        }
+
+    }
+}
---
+++ SmartKey/Log/LogPersistence/TestPersistenceLog/TestLogPersistence.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using SmartKey.Blacklist;
+using SmartKey.Controller;
+using SmartKey.Controller.Controller.Interfaces;
+using SmartKey.ImpostazioneTrasferimento;
+using SmartKey.Log.ControllerLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartKey.Log.TestLog
+{
+    [TestFixture]
+    class TestLogPersistence
+    {
+        private IController impostazioneController;
+        private IObserver logController;
+        private IController blacklistController;
+
+        private void Init()
+        {
+            blacklistController = new BlackListController();
+            impostazioneController = new ImpostazioneTrasferimentoController();
+            logController = new LogController();
+            impostazioneController.ToLog += logController.UpdateLog;
+            blacklistController.ToLog += logController.UpdateLog;
+
+        }
+
+        [TestCase]
+        public void TestScritturaImpostazione()
+        {
+            Init();
+            ((IGestoreImpostazione)impostazioneController).AddImpostazione(
+                new ImpostazioneTrasferimento.ImpostazioneTrasferimento("mydir", "yourDir"));
+        }
+
+        [TestCase]
+        public void TestScritturaBlacklist()
+        {
+            Init();
+            ((IGestoreBlacklist)blacklistController).AggiungiUtente("riccardo");
+        }
+
+    }
+}
