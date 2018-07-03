34ef30b4a19c8eef9d895964d39730535cf9b770
Riccardo Vasumini <riccardo.vasumini2@studio.unibo.it>
2018-07-03 18:28:24 +0200

    Modificato il campo Text di BaseForm in "SmartKey"

--- SmartKey/BaseForm.Designer.cs
+++ SmartKey/BaseForm.Designer.cs
@@ -151,12 +151,13 @@ namespace SmartKey
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 372);
             this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "BaseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Home Smartkey";
+            this.Text = "SmartKey";
             this.Load += new System.EventHandler(this.BaseForm_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
