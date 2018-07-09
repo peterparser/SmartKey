856338b801f8481705cbf818367ea17a2c5ef301
Massimo Schembri <massimo.schembri@studio.unibo.it>
2018-07-09 09:35:11 +0200

    Manca l'implementazione e il testing della protezione, inserita entry sistema il parsing di entry sistema.

---
+++ SmartKey/Controller/ProtezioneDatiController.cs
@@ -0,0 +1,29 @@
+﻿using SmartKey.Controller.Controller.Interfaces;
+using SmartKey.Log.ModelLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartKey.Controller
+{
+    public class ProtezioneDatiController : IGestoreProtezioneDati
+    {
+        public event EventHandler<ActionCompletedEvent> ToLog;
+
+        public void ProteggiCartella(string path)
+        {
+            if (Directory.Exists(path))
+            {
+
+                ActionCompletedEvent args = new ActionCompletedEvent
+                {
+                    ToEntry = EntryFactory.CreateEntry(this, "protetta cartella")
+                };
+                ToLog?.Invoke(this, args);
+            }
+        }
+    }
+}
