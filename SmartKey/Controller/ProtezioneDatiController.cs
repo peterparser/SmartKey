8b731b1fc5933c21d1a4b1c02a540d29edb3d6f2
Massimo Schembri <massimo.schembri@studio.unibo.it>
2018-07-09 14:32:17 +0200

    Modifica, la cartella non sempre viene creata, potrebbe già esistere.

--- SmartKey/Controller/ProtezioneDatiController.cs
+++ SmartKey/Controller/ProtezioneDatiController.cs
@@ -19,42 +19,44 @@ namespace SmartKey.Controller
         {
             if (!Directory.Exists(path))
             {
-                DirectoryInfo dInfo = new DirectoryInfo(path);
-                DirectorySecurity security = dInfo.GetAccessControl();
-
-                // Bisogna rimuovere una regola protetta, quindi bisogna anche rimuovere l'ereditarietà, da qui true e false
-                security.SetAccessRuleProtection(true, false);
-
-                // Prendo tutte le regole presenti.
-                AuthorizationRuleCollection rules = security.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
-                //Ciclo sulle regole e le rimuovo tutte
-                foreach (FileSystemAccessRule rule in rules)
-                {
-                    security.RemoveAccessRule(rule);
-                }
-                // security.RemoveAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, AccessControlType.Allow));
-
-                //Inserisco la regola che consente il full control al proprietario
-                Console.WriteLine(WindowsIdentity.GetCurrent().Name);
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.Delete, AccessControlType.Deny));
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.DeleteSubdirectoriesAndFiles, AccessControlType.Allow));
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.CreateDirectories, AccessControlType.Allow));
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.CreateFiles, AccessControlType.Allow));
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.ReadAndExecute, AccessControlType.Allow));
-                security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.Modify, AccessControlType.Allow));
-
-
-
-                //Applico le regole alla cartella
-                Directory.SetAccessControl(path, security);
-
-                ActionCompletedEvent args = new ActionCompletedEvent
-                {
-                    ToEntry = EntryFactory.CreateEntry(this, "protetta cartella "+path)
-                };
-                ToLog?.Invoke(this, args);
-
+                Directory.CreateDirectory(path);
             }
+            
+            DirectoryInfo dInfo = new DirectoryInfo(path);
+            DirectorySecurity security = dInfo.GetAccessControl();
+
+            // Bisogna rimuovere una regola protetta, quindi bisogna anche rimuovere l'ereditarietà, da qui true e false
+            security.SetAccessRuleProtection(true, false);
+
+            // Prendo tutte le regole presenti.
+            AuthorizationRuleCollection rules = security.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+            //Ciclo sulle regole e le rimuovo tutte
+            foreach (FileSystemAccessRule rule in rules)
+            {
+                security.RemoveAccessRule(rule);
+            }
+            // security.RemoveAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, AccessControlType.Allow));
+
+            //Inserisco la regola che consente il full control al proprietario
+            Console.WriteLine(WindowsIdentity.GetCurrent().Name);
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.Delete, AccessControlType.Deny));
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.DeleteSubdirectoriesAndFiles, AccessControlType.Allow));
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.CreateDirectories, AccessControlType.Allow));
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.CreateFiles, AccessControlType.Allow));
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.ReadAndExecute, AccessControlType.Allow));
+            security.AddAccessRule(new FileSystemAccessRule("Everyone", FileSystemRights.Modify, AccessControlType.Allow));
+
+
+
+            //Applico le regole alla cartella
+            Directory.SetAccessControl(path, security);
+
+            ActionCompletedEvent args = new ActionCompletedEvent
+            {
+                ToEntry = EntryFactory.CreateEntry(this, "protetta cartella "+path)
+            };
+            ToLog?.Invoke(this, args);
+
         }
     }
 }
