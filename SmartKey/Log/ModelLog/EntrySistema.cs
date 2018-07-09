f0c3fd573d69bc56edabd22aa0d30dcaa01f8013
Arianna Solini <arianna.solini@studio.unibo.it>
2018-07-09 12:11:52 +0200

    Sistemata Entry Sistema

---
+++ SmartKey/Log/ModelLog/EntrySistema.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartKey.Log.ModelLog
+{
+    public class EntrySistema : Entry
+    {
+        public EntrySistema(string operazione) : base(operazione)
+        {
+        }
+
+        public EntrySistema(string operazione, DateTime date) : base(operazione, date)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "" + base.DataOra + "\tSistema\t" + base.Operazione;
+        }
+    }
+}
